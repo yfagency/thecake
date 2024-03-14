21c55555ecadf379ce7976be4c286853c733e42e
SirBruno <bruno.webjr@gmail.com>
2024-03-14 14:36:40 +0900

    Setup CSS Modules

--- app/root.jsx
+++ app/root.jsx
@@ -16,6 +16,7 @@ import favicon from './assets/favicon.svg';
 import resetStyles from './styles/reset.css';
 import appStyles from './styles/app.css';
 import {Layout} from '~/components/Layout';
+import {cssBundleHref} from '@remix-run/css-bundle';
 
 /**
  * This is important to avoid re-fetching root queries on sub-navigations
@@ -37,6 +38,7 @@ export const shouldRevalidate = ({formMethod, currentUrl, nextUrl}) => {
 
 export function links() {
   return [
+    ...(cssBundleHref ? [{rel: 'stylesheet', href: cssBundleHref}] : []),
     {rel: 'stylesheet', href: resetStyles},
     {rel: 'stylesheet', href: appStyles},
     {
